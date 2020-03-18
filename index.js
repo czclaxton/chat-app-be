d30b066cf79b5656028bb4782b89cd49a327a02d
Connor Claxton <czclaxton@gmail.com>
2020-03-18 13:57:39 -0500

    update

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ const http = require("http");
 
 const PORT = process.env.PORT || 5000;
 
+const router = require("./router");
+
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on("connection", socket => {
+  console.log("New Connection!!");
+
+  socket.on("disconnect", () => {
+    console.log("LEAVING NOW");
+  });
+});
+
+app.use(router);
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
